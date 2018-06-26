fafc9334e78b09969ee49d825b5a567b636883d4
SteveJokes <1466736758@qq.com>
2018-06-26 21:53:37 +0800

    new post

--- push.sh
+++ push.sh
@@ -5,6 +5,7 @@
 # Created Time: 2018年06月23日 星期六 22时39分07秒
 #########################################################################
 #!/bin/bash
+git pull origin master
 git add ./*
 git commit -m "new post"
 git push origin master
